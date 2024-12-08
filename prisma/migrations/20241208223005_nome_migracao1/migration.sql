cc3c23de32c1f1e141dd56f31f8cfab027babeae
yjjpo <agnesyasmini11@gmail.com>
2024-12-08 22:38:01 +0000

    crud

--- prisma/migrations/20241208223005_nome_migracao1/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Livro" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "Titulo" TEXT NOT NULL,
-    "Autor" TEXT NOT NULL,
-    "AnoPublicacao" INTEGER NOT NULL,
-    "Genero" TEXT NOT NULL
-);
