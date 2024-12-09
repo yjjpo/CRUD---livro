d3109c9141c35962d364ed046f9d708a42ac2e69
yjjpo <agnesyasmini11@gmail.com>
2024-12-09 13:06:54 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,6 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use('/api', livroRoutes);
-
-
-app.get('/', (req, res) => {
-    res.send('Servidor rodando!');
-  });
   
   app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
